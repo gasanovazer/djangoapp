e0f5fbc068d9d6ce73714dfe07b2e72be16d77b4
x0gae <gasanovazer@outlook.com>
2023-12-31 00:58:03 -0800

    The main app is ready pages index and about

--- main/views.py
+++ main/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
 # Create your views here.
+
+def index(request):
+    context: dict[str, str] = {
+        'title': 'Main',
+        'content': 'Главная страница магазина',
+        'list': ['first', 'second'],
+        'dict': {'first': 1},
+        'is_authenticated': False,
+    }
+    return render(request, 'main/index.html', context)
+
+def about(request):
+    return HttpResponse('About page')
