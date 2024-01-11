c05cb850e838101d2ccd4250c24ad515c4777265
x0gae <gasanovazer@outlook.com>
2024-01-11 05:41:28 -0800

    Registration form and controller are ready

--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
 from django.urls import reverse
 from django.contrib import auth
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 # Create your views here.
 
@@ -26,8 +26,18 @@ def login(request):
     return render(request, 'users/login.html', context)
 
 def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
     context={
-        'title': 'Home - Регистрация'
+        'title': 'Home - Регистрация',
+        'form': form,
     } 
 
     return render(request, 'users/registration.html', context)
@@ -39,4 +49,5 @@ def profile(request):
     return render(request, 'users/profile.html', context)
 
 def logout(request):
-    pass
+    auth.logout(request)
+    return redirect(reverse('main:index'))
